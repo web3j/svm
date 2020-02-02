c768730bfdb196c479bc2dae88693ab9643e6fdb
Joshua Richardson <joshua@richardson.tech>
2020-02-02 23:48:31 +0000

    Gitignore and native image generation for macos

--- generate_native_image.sh
+++ generate_native_image.sh
@@ -3,4 +3,4 @@
 ./gradlew build
 ./gradlew shadowJar
 cd "build/libs" || exit
-native-image -cp svm-1.0-SNAPSHOT-all.jar tech.richardson.svm.MainKt --no-fallback --enable-https
+native-image -cp svm-1.0-SNAPSHOT-all.jar tech.richardson.svm.MainKt --no-fallback --enable-https --enable-http
